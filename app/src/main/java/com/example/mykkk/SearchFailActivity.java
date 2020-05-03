f6540b15e7d2b401e355a2f55772052c6d63db8f
anyuan <380666201@qq.com>
2020-05-03 20:41:07 +0800

    addccc

---
+++ app/src/main/java/com/example/mykkk/SearchFailActivity.java
@@ -0,0 +1,14 @@
+package com.example.mykkk;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SearchFailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search_fail);
+    }
+}
---
+++ app/src/main/java/com/example/mykkk/SendPageActivity.java
@@ -0,0 +1,14 @@
+package com.example.mykkk;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SendPageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_send_page);
+    }
+}
